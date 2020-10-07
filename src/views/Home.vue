5b0a88b69edfc9afe7e818b1f6ffb971c2fd7467
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-08 07:17:19 +0800

    Home的结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>我的UI</h1>
       <h2>这是一个厉害的UI框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com/">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
