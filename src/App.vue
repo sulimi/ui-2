16ed25ddd5f109c299000bd5d5de27b9e094bbc5
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-07 17:02:09 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <One></One>
+</template>
+
+<script>
+  import One from './components/One.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      One: One
+    }
+  };
+</script>
