4fbafebf86899486a32633fbf0424d765cda97ae
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-08 19:41:25 +0800

    创建button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,20 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-    Button
+    <ButtonTool @click="func">我是按钮</ButtonTool>
   </div>
 </template>
+<script lang="ts">
+  import ButtonTool from '../lib/ButtonTool.vue';
+
+  export default {
+    components: {ButtonTool},
+    setup(){
+      const func=()=>{
+        console.log('被点击了');
+      }
+      return {func}
+    }
+  };
+</script>
---
+++ src/lib/ButtonTool.vue
@@ -0,0 +1,15 @@
+<template>
+    <button v-bind=$attrs>
+      <slot/>
+    </button>
+</template>
+<script lang="ts">
+
+
+  export default {
+    props:{},
+    setup(props, context) {
+
+    }
+  };
+</script>
