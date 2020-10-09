5e0be30c100571921f6fcb06eafa50429737875e
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-09 21:28:03 +0800

    内容切换Tab插槽内容无法更新（Vue 3）bug

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div>Tabs示例</div>
   <h1>示例1</h1>
-  <TabsTool>
+  <TabsTool v-model:selected="selectedvalue">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </TabsTool>
@@ -9,9 +9,14 @@
 <script lang="ts">
   import Tab from '../lib/Tab.vue';
   import TabsTool from '../lib/TabsTool.vue';
+  import {ref} from 'vue';
 
   export default {
     components: {Tab, TabsTool},
+    setup() {
+      const selectedvalue = ref('导航1');
+      return {selectedvalue};
+    }
 
   };
 </script>
--- src/lib/TabsTool.vue
+++ src/lib/TabsTool.vue
@@ -1,12 +1,27 @@
 <template>
-  <div v-for="(title,index) in titles" :key="index">{{title}}</div>
-  <component v-for='(slot,index) in defaults'
-             :key="index" :is="defaults[0]"/>
+  <div class="sumi-tabs">
+    <div class="sumi-tabs-nav">
+      <div class="sumi-tabs-nav-item"
+           :class="{selected: t===selected}"
+           @click="select(t)"
+           v-for="(t,index) in titles" :key="index">{{t}}
+      </div>
+    </div>
+    <div class="sumi-tabs-content">
+      <component :is="selectConten"></component>
+    </div>
+  </div>
 </template>
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {computed} from 'vue';
 
   export default {
+    props: {
+      selected: {
+        type: String
+      }
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach(tag => {
@@ -14,13 +29,52 @@
           throw new Error('Tabs 子标签必须是 Tab');
         }
       });
+      const selectConten = computed(() => {
+        const vNodeValue = defaults.filter(tag => {
+          return tag.props.title === props.selected;
+        })[0];
+        return vNodeValue;
+      });
+
       const titles = defaults.map(tag => {
         return tag.props.title;
       });
-      return {defaults, titles};
+      const select = (title: string) => {
+        console.log(selectConten);
+        context.emit('update:selected', title);
+      };
+
+      return {defaults, titles, select, selectConten};
     }
   };
 </script>
 <style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .sumi-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
 
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
+      }
+    }
+
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
