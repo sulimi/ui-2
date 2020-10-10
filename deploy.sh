2f80558ce9def1181c13cabde74719561324df64
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-11 04:56:18 +0800

    GitHub改版了，master变成main

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin origin git@github.com:sulimi/sumi-ui.git &&
-git push -u origin main &&
-cd ..
+git remote add origin git@github.com:sulimi/sumi-ui.git &&
+git push -f -u origin main &&
+cd -
 echo https://sulimi.github.io/sumi-ui/index.html
