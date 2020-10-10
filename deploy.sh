ea6db08550264abafc3bbaa4e2589d2d432dae8e
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-11 04:40:42 +0800

    添加一键部署脚本

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:sulimi/sumi-ui-website.git &&
-git push -f -u origin master &&
-cd -
-echo https://sulimi.github.io/sumi-ui-website/index.html
+git remote add origin origin git@github.com:sulimi/sumi-ui.git &&
+git push -u origin master &&
+cd ..
+echo https://sulimi.github.io/sumi-ui/index.html
